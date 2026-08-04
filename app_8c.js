db16f4a6091e6ffa5d05927ba75415c1639e8de9
mochapulse <mochapulse@users.noreply.github.com>
2026-08-04 17:48:44 +0000

    docs: deploy doxygen 31da07b8c68130f9b74a7ba39d3c43f19c7f58e7 31da07b8c68130f9b74a7ba39d3c43f19c7f58e7

---
+++ app_8c.js
@@ -0,0 +1,7 @@
+var app_8c =
+[
+    [ "HEALTH_POLL_MS", "app_8c.html#a4345be2dcb6ddeaaf65372cbe532b791", null ],
+    [ "IP_WAIT_TIMEOUT_MS", "app_8c.html#a289c6aa8d9aeec80b2e2e9f92a700c19", null ],
+    [ "app_main", "app_8c.html#a630544a7f0a2cc40d8a7fefab7e2fe70", null ],
+    [ "TAG", "app_8c.html#a5a85b9c772bbeb480b209a3e6ea92b4c", null ]
+];
---
+++ web__util_8h.js
@@ -0,0 +1,7 @@
+var web__util_8h =
+[
+    [ "web_health", "web__util_8h.html#a71620b2ae09ea356192cbd1810788d4b", null ],
+    [ "web_init", "web__util_8h.html#a0863e0f49b763624af7de46c1485a702", null ],
+    [ "web_start", "web__util_8h.html#ad170a916d69357f1f3354b455129631e", null ],
+    [ "web_stop", "web__util_8h.html#ab40dec67bd9f60744e9dd52104030292", null ]
+];
